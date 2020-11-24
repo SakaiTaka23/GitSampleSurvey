70f8b56485148ed0980892ff1252098536b388f3
SakaiTaka23 <tt3.sakai@gmail.com>
2020-11-24 18:31:06 +0900

    結果をグラフ表示できるようにした

--- analyze_result.py
+++ analyze_result.py
@@ -1,5 +1,33 @@
-import pandas
-from pandas import plotting
+import pandas as pd
+import matplotlib.pyplot as plt
 
-data = pandas.read_csv('result.csv',sep=';', na_values=".")
-print(data)
+data = pd.read_csv('result.csv',index_col=0)
+
+# print("平均")
+# commit = data['Commit'].mean()
+# print(commit)
+# star = data['Star'].mean()
+# print(star)
+# pub_repo = data['Public_repo'].mean()
+# print(pub_repo)
+
+# print("中央値")
+# commit = data['Commit'].median()
+# print(commit)
+# star = data['Star'].median()
+# print(star)
+# pub_repo = data['Public_repo'].median()
+# print(pub_repo)
+
+# print("標準偏差")
+# commit = data['Commit'].std()
+# print(commit)
+# star = data['Star'].std()
+# print(star)
+# pub_repo = data['Public_repo'].std()
+# print(pub_repo)
+
+plt.figure()
+data['Public_repo'].hist()
+plt.savefig('result.jpg')
+plt.close('all')
