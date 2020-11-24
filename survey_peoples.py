269373cab5384a03d834f96d0ee89f0166ad7664
SakaiTaka23 <tt3.sakai@gmail.com>
2020-11-24 10:11:51 +0900

    任意人数調査を可能にした

---
+++ survey_peoples.py
@@ -0,0 +1,16 @@
+from csv_process import write_result
+from dotenv import load_dotenv
+import os
+from survey.survey_one_people import survey_one_people
+
+
+def survey_peoples(survey_people):
+    load_dotenv()
+    api_token = os.environ['api_token']
+    i = 0
+    while i < survey_people:
+        result = survey_one_people(api_token)
+        write_result(result)
+        i += 1
+
+survey_peoples(10)
