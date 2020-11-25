e40bc4a4c68df966e7478fb6a3dd8d8aba67be66
SakaiTaka23 <tt3.sakai@gmail.com>
2020-11-25 09:52:56 +0900

    無効なidだった場合の例外処理を実装

--- survey/get_random_name.py
+++ survey/get_random_name.py
@@ -1,9 +1,25 @@
 import random
 import requests
+import sys
 
 def get_random_name(api_token):
-    rand_id = random.randint(1,70000000)
-    url = "https://api.github.com/user/"
-    api = requests.get(url+str(rand_id)+"?access_token="+api_token).json()
+    try_count = 1
+    api = ""
+    while try_count <= 3:
+        rand_id = make_random_id()
+        url = "https://api.github.com/user/"+str(rand_id)+"?access_token="+api_token
+        api = requests.get(url)
+        if api.status_code == requests.codes.ok:
+            api = api.json()
+            break
+        elif try_count == 3:
+            print("[Error] tried 3 times but failed...")
+            sys.exit()
+        else:
+            try_count += 1
     name = api['login'].replace("'","")
     return name
+
+def make_random_id():
+    rand_id = random.randint(1,70000000)
+    return rand_id
