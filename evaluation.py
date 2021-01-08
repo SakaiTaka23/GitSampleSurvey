fe6d470a63e671b9e22770285110bd6ac6ea2a66
SakaiTaka23 <tt3.sakai@gmail.com>
2021-01-08 15:25:02 +0900

    一人分の調査をできるようにした

--- evaluation.py
+++ evaluation.py
@@ -1,58 +1,63 @@
-from dotenv import load_dotenv
-import os
-from github import Github
-import requests
-import json
+import math
 
-load_dotenv()
-token = os.environ['api_token']
-
-url = 'https://api.github.com/users/'
-name = 'SakaiTaka23'
-
-url = url + name
-r = requests.get(url)
-data = json.loads(r.text)
-
-public_repos = data['public_repos']
-followers = data['followers']
-print("public_repo: "+str(public_repos))
-print("followers: "+str(followers))
-
-
-url = 'https://api.github.com/search/issues?q=+type:pr+user:' + \
-    name + '&sort=created&order=asc'
-r = requests.get(url)
-data = json.loads(r.text)
-
-pull_requests = data['total_count']
-print("pull_requests: "+str(pull_requests))
-
-g = Github(token)
-issues = g.get_user().get_user_issues()
-print("issues: "+str(issues.totalCount))
-
-
-def repo_process(full_repo_name, g):
-    repo = g.get_repo(full_repo_name)
-    # print(dir(repo))
-    total_star = repo.stargazers_count
-    try:
-        total_commit = repo.get_commits().totalCount
-    except Exception:
-        # リポジトリが空の時に発生
-        total_commit = 0
-    return total_commit, total_star
-
-
-commit_sum = 0
+public_repo = 16
+commit_sum = 455
+issues = 0
+pull_requests = 15
 star_sum = 0
-for repo in g.get_user(name).get_repos(type='public'):
-    if repo.fork:
-        continue
-    counts = repo_process(repo.full_name, g)
-    commit_sum += counts[0]
-    star_sum += counts[1]
+followers = 1
 
-print("commit_sum: "+str(commit_sum))
-print("star_sum: "+str(star_sum))
+PUBLIC_REPO_OFFSET = 1.18
+COMMIT_OFFSET = 1.16
+ISSUES_OFFSET = 1.13
+PULL_REQUESTS_OFFSET = 1.10
+STARS_OFFSET = 0.92
+FOLLOWERS_OFFSET = 0.91
+
+# 元のプログラムの値
+# PUBLIC_REPO_OFFSET = 1
+# COMMIT_OFFSET = 1.65
+# ISSUES_OFFSET = 1
+# PULL_REQUESTS_OFFSET = 0.5
+# STARS_OFFSET = 0.75
+# FOLLOWERS_OFFSET = 0.45
+ALL_OFFSETS = PUBLIC_REPO_OFFSET+COMMIT_OFFSET+ISSUES_OFFSET + \
+    PULL_REQUESTS_OFFSET+STARS_OFFSET+FOLLOWERS_OFFSET
+
+RANK_S_VALUE = 1
+RANK_DOUBLE_A_VALUE = 25
+RANK_A2_VALUE = 45
+RANK_A3_VALUE = 60
+RANK_B_VALUE = 100
+TOTAL_VALUES = RANK_S_VALUE + RANK_DOUBLE_A_VALUE + \
+    RANK_A2_VALUE + RANK_A3_VALUE + RANK_B_VALUE
+
+score = (
+    public_repo*PUBLIC_REPO_OFFSET +
+    commit_sum*COMMIT_OFFSET +
+    issues * ISSUES_OFFSET +
+    pull_requests * PULL_REQUESTS_OFFSET +
+    star_sum * STARS_OFFSET +
+    followers * FOLLOWERS_OFFSET
+) / 100
+
+
+def normalcdf(mean, sigma, to):
+    z = (to - mean) / math.sqrt(2 * sigma * sigma)
+    t = 1 / (1 + 0.3275911 * abs(z))
+    a1 = 0.254829592
+    a2 = -0.284496736
+    a3 = 1.421413741
+    a4 = -1.453152027
+    a5 = 1.061405429
+    erf = 1 - ((((a5 * t + a4) * t + a3) * t + a2)
+               * t + a1) * t * math.exp(-z * z)
+    sign = 1
+    if z < 0:
+        sign = -1
+    return (1 / 2) * (1 + sign * erf)
+
+
+normalizedScore = normalcdf(score, TOTAL_VALUES, ALL_OFFSETS) * 100
+print("score: ")
+print(normalizedScore)
